c79511e269bfff4d480a950fcd0f8e2b31eaca08
Alex Guerrero <alex.guerrero@sngular.com>
2024-10-21 15:29:19 +0200

    fix: change api key

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,7 +8,7 @@ export class GifsService {
   public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
-  private apiKey:       string = 'dGMJW12nlHSz46O3sy81BZFryoPshmUk';
+  private apiKey:       string = '0nxHVoD0xlvaOZ1AgMhoL5g7SliIoLUO';
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
   constructor( private http: HttpClient ) {
